dd4995b38da6066d977a2367fcb6152d2c56ecb4
SwapnaM02 <m.v.swapnareddy@gmail.com>
2023-04-20 15:43:06 +0530

    igniting our app using parcel

---
+++ 02Ignitingourapp/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+
+const heading=React.createElement("h1",{id:"title"},"heading1 swapna");
+const heading2=React.createElement("h2",{id:"title"},"heading2");
+const container=React.createElement("div",{id:"container"},[heading,heading2]);
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
+
+
